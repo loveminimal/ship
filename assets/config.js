83e543e9f5168c95155c35dfa1674550d39ad0d5
jack <loveminimal@163.com>
2023-07-28 10:37:44 +0800

    Updates.

--- assets/config.js
+++ assets/config.js
@@ -6,13 +6,13 @@ const config = {
 	engine: 'by',
 	// Set frequents nav
 	nav: {
-		Virgo: '//loveminimal.github.io',
-		Nav: '//loveminimal.github.io/nav',
-		Archive: '//loveminimal.github.io/archive',
-		Blog: '//loveminimal.github.io/posts',
+		Virgo: '//walkssi.com',
+		Nav: '//walkssi.com/nav',
+		Archive: '//walkssi.com/archive',
+		Blog: '//walkssi.com/posts',
 		// Github: '//github.com/loveminimal',
 		// Bilibili: '//bilibili.com',
 		// Youtube: '//youtube.com',
-		Mine: '//loveminimal.github.io/about'
+		Mine: '//walkssi.com/about'
 	}
 }
